691523171a38c288fe8306ca393f7c9d5ae28267
Trend20 <enockomondi305@gmail.com>
2021-06-07 14:34:52 +0300

    initial commit

---
+++ contracts/Hello.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+
+// hello world contract
+contract Hello {
+  function sayHello() pure public returns(string memory){
+    return "Hello World";
+  }
+}
